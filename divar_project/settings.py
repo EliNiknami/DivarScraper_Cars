f38b91ff7067d284a46db0d1dc27c5a559d0ea94
EliNiknami <133388663+EliNiknami@users.noreply.github.com>
2026-02-02 22:42:17 +0330

    Update settings.py

--- divar_project/settings.py
+++ divar_project/settings.py
@@ -1,1 +1,18 @@
+BOT_NAME = "divar_project"
 
+SPIDER_MODULES = ["divar_project.spiders"]
+NEWSPIDER_MODULE = "divar_project.spiders"
+
+ADDONS = {}
+
+# مهم
+ROBOTSTXT_OBEY = False
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 1
+
+ITEM_PIPELINES = {
+    "divar_project.pipelines.DivarProjectPipeline": 300,
+}
+
+FEED_EXPORT_ENCODING = "utf-8"
